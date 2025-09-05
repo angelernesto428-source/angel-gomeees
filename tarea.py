6e7975e264a89e0e295f5e828c46ade5f260d6e5
maluco <angelernesto428@gmail.com>
2025-09-05 16:53:47 -0400

    Add files via upload

---
+++ tarea.py
@@ -0,0 +1,11 @@
+start = print("estas de vacaciones en tu casa del bosque, y escuchas pasos afuera")
+opcion_1 = input("que haces?? : SALES A VER QUE PASA o IGNORAS EL SUCESO").lower()
+if opcion_1 == "SALES":
+    print("sales de tu casa y ves la silueta de una persona,que a medida que se acerca cambia de forma")
+    opcion_1_1= input("\n que haces ENTRAS A LA CASA Y TE ESCONDES o TE ACERCAS \n : ").lower()
+    if opcion_1_1 == ""
+
+
+elif opcion_1 == "IGNORAS":
+
+
