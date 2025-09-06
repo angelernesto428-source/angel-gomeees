a65340b5e28a5e137724763988da3db4f8bf8194
maluco <angelernesto428@gmail.com>
2025-09-06 00:14:52 -0400

    Add files via upload

---
+++ aventuranose.py
@@ -0,0 +1,7 @@
+start = print("te despiertas en una cueva oscura con una antorcha y una nota.")
+opcion_1 = input("que haces?? :  REVISAR NOTA o ENCENDER ANTORCHA").lower()
+if opcion_1 == "revisar nota":
+    print("las nota dice que te dirigas hacia la derecha")
+    print("le haces caso a la nota y vas a donde dijo(estupido)")
+    print("escuchas un ruido raro,suena peligroso que haces??"
+    ifopcion_1_1= input("\n AGACHARTE o CORRER \n : ").lower()
