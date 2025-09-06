7fc0109ed0c13cbf66c101b658fb6cf546d5801a
maluco <angelernesto428@gmail.com>
2025-09-06 14:01:02 -0400

    Actualizar aventuranose.py

--- aventuranose.py
+++ aventuranose.py
@@ -2,6 +2,11 @@ start = print("te despiertas en una cueva oscura con una antorcha y una nota.")
 opcion_1 = input("que haces?? :  REVISAR NOTA o ENCENDER ANTORCHA").lower()
 if opcion_1 == "revisar nota":
     print("las nota dice que te dirigas hacia la derecha")
-    print("le haces caso a la nota y vas a donde dijo(estupido)")
-    print("escuchas un ruido raro,suena peligroso que haces??"
-    ifopcion_1_1= input("\n AGACHARTE o CORRER \n : ").lower()
+    print("le haces caso a la nota y vas a donde dijo")
+    print("escuchas un ruido raro,suena peligroso que haces??")
+    opcion_1_1= input("\n AGACHARTE o CORRER \n : ").lower()
+    if opcion1_1 == "agacharte":
+    	print("te agachas y gracias a eso esquivas")
+    	print("esquivas la  sigues a delante y te encuentras")
+    
+ 
