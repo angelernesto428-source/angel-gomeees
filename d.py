bcfca35e61890ecd82abf24c7d2789aaa373e28e
maluco <angelernesto428@gmail.com>
2025-09-12 16:56:55 -0400

    Add files via upload

---
+++ d.py
@@ -0,0 +1,8 @@
+mi_diccionario = {
+    "nombre": "angel",
+    "apellido": "gomez",
+    "color favorito": "azul",
+}
+
+for clave , valor in mi_diccionario.items():
+    print(f"\nclave: {clave}, valor: {valor}")
